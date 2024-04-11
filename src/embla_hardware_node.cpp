1f27bc61ac105ce749f2a8895a47f1d84c82da09
Jens Willy Johannsen <jens@jenswilly.dk>
2024-04-11 13:04:54 +0200

    First ROS2 commit. Commented out a lot of stuff.

--- src/embla_hardware_node.cpp
+++ src/embla_hardware_node.cpp
@@ -1,36 +1,37 @@
 /*
-* Copyright 2019 Jens Willy Johannsen <jens@jwrobotics.com>, JW Robotics
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-* JWR-01 Embla Hardware driver/controller
-*/
+ * Copyright 2019 Jens Willy Johannsen <jens@jwrobotics.com>, JW Robotics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * JWR-01 Embla Hardware driver/controller
+ */
 
-#include "embla_hardware/embla_hardware.h"
-#include "controller_manager/controller_manager.h"
-#include "ros/callback_queue.h"
-#include <robot_state_publisher/robot_state_publisher.h>
-#include <kdl_parser/kdl_parser.hpp>
+// #include "embla_hardware/embla_hardware.h"
+#include "rclcpp/rclcpp.hpp"
+// #include "controller_manager/controller_manager.h"
+// #include "ros/callback_queue.h"
+// #include <robot_state_publisher/robot_state_publisher.h>
+// #include <kdl_parser/kdl_parser.hpp>
 #include <boost/chrono.hpp>
 
 typedef boost::chrono::steady_clock time_source;
-
+#if 0
 /**
 * Control loop, not realtime safe
 */
@@ -144,5 +145,23 @@ int main( int argc, char *argv[] )
 	// Process remainder of ROS callbacks separately, mainly ControlManager related
 	ros::spin();
 
+	return 0;
+}
+#endif
+
+class EmblaHardwareNode : public rclcpp::Node
+{
+public:
+	EmblaHardware() : Node("embla_hardware")
+	{
+		RCLCPP_INFO(this->get_logger(), "EmblaHardware constructor");
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<EmblaHardwareNode>());
+	rclcpp::shutdown();
 	return 0;
 }
